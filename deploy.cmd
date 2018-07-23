f60b800807143b386aa3156198d2f3c1d88a433f
Jack Stromberg <jstrom@microsoft.com>
2018-07-23 16:02:49 -0500

    Update deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -1,13 +1,7 @@
 :: 1. Install Wheel
 echo Configure Wheel
 D:\home\python364x64\python.exe -m pip install wheel
-IF !ERRORLEVEL! NEQ 0 (
-  echo %ERRORLEVEL%
-  echo Wheel for python has failed to install.
-  goto error
-)
 
 :: 2. Install packages
 echo Pip install requirements.  
-D:\home\python364x64\python.exe -m pip install --upgrade -r D:\home\site\wwwroot\requirements.txt  
-IF !ERRORLEVEL! NEQ 0 goto error
+D:\home\python364x64\python.exe -m pip install --upgrade -r D:\home\site\wwwroot\requirements.txt
